8a82cae0616dccb8fdd9595d930f780cb762ec83
Rajveer <rajveerchauhan936@gmail.com>
2026-02-22 22:40:18 +0530

    some things are added in analytics screen

--- lib/pages/analytics.dart
+++ lib/pages/analytics.dart
@@ -1,10 +1,234 @@
+import 'package:finova_ai/providers/total_expense_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Analytics extends StatelessWidget {
+class Analytics extends ConsumerStatefulWidget {
   const Analytics({super.key});
 
   @override
+  ConsumerState<Analytics> createState() => _AnalyticsState();
+}
+
+class _AnalyticsState extends ConsumerState<Analytics> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Analytics")));
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+
+    final totalExpense = ref.watch(totalExpenseProvider);
+
+    return Scaffold(
+      backgroundColor: Colors.grey.shade50,
+      body: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(18),
+                bottomRight: Radius.circular(18),
+              ),
+              color: Color(0xFF4A90FF),
+              boxShadow: [
+                BoxShadow(
+                  color: const Color.fromARGB(31, 112, 112, 112),
+                  blurRadius: 12.0,
+                  spreadRadius: 0,
+                ),
+              ],
+            ),
+            width: screenWidth * 1,
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.06),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: screenHeight * 0.08),
+                  RichText(
+                    text: TextSpan(
+                      text: 'Analytics',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: 'SFProText',
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+
+                  SizedBox(height: screenHeight * 0.02),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: screenWidth * 0.4,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          color: Color.fromARGB(255, 100, 159, 255),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.01,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: screenHeight * 0.002),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: screenWidth * 0.04,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 6),
+                                    RichText(
+                                      text: TextSpan(
+                                        text: 'Total Spent',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'SFProText',
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 12),
+                                    RichText(
+                                      text: TextSpan(
+                                        text:
+                                            '₹ ${totalExpense.toStringAsFixed(2)}', // Replace with actual total expenses
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'SFProText',
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 6),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: screenWidth * 0.4,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.white),
+                          color: Color.fromARGB(255, 100, 159, 255),
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: screenHeight * 0.01,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(height: screenHeight * 0.002),
+                              Padding(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: screenWidth * 0.04,
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(height: 6),
+                                    RichText(
+                                      text: TextSpan(
+                                        text: 'Avg/Month',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'SFProText',
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w300,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 12),
+                                    RichText(
+                                      text: TextSpan(
+                                        text:
+                                            '₹ 26,242', // Replace with actual total expenses
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                          fontFamily: 'SFProText',
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ),
+                                    SizedBox(height: 6),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: screenHeight * 0.03),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(height: screenHeight * 0.02),
+          Container(
+            width: screenWidth * 0.92,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 237, 247, 255),
+              border: Border.all(color: Colors.lightBlue.shade100, width: 1.5),
+              borderRadius: BorderRadius.circular(18),
+            ),
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: screenWidth * 0.05,
+                vertical: screenHeight * 0.02,
+              ),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.asset('assets/idea.png', height: screenHeight * 0.04),
+                  SizedBox(width: screenWidth * 0.04),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      RichText(
+                        text: TextSpan(
+                          text: 'AI Insights',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 18,
+                            fontFamily: 'SFProText',
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: screenHeight * 0.004),
+                      RichText(
+                        text: TextSpan(
+                          text:
+                              'Your spending increased by 8.5% this month.\nTravel is your highest expense category.',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 12,
+                            fontFamily: 'SFProText',
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
